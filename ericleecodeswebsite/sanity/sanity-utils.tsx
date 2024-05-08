c0f9bea6f969850beb807df03d239130a0afaeaf
Eric Lee <ericleecodes@gmail.com>
2024-05-08 16:35:05 -0700

    Setup TypeScript types

--- ericleecodeswebsite/sanity/sanity-utils.tsx
+++ ericleecodeswebsite/sanity/sanity-utils.tsx
@@ -1,6 +1,7 @@
+import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 
-export async function getProjects() {
+export async function getProjects(): Promise<Project[]> {
   const client = createClient({
     projectId: "kv2q6jn7",
     dataset: "production",
