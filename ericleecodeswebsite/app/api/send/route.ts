3633652d3a205f37ea52ad6e712ad9b2b387c280
Eric Lee <ericleecodes@gmail.com>
2024-06-07 16:24:00 -0700

    Reorganize email

--- ericleecodeswebsite/app/api/send/route.ts
+++ ericleecodeswebsite/app/api/send/route.ts
@@ -1,25 +1,7 @@
-import ContactMe from "@/app/emails/ContactMe";
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import mailgun from "mailgun-js";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function POST(request: Request) {
-  const { email, firstName } = await request.json();
-  try {
-    const { data } = await resend.emails.send({
-      from: { email },
-      to: email,
-      subject: "EricLeeCodes Contact",
-      react: ContactMe({ firstName }),
-    });
-    return NextResponse.json({ data });
-  } catch (error) {
-    let errorMessage = "An unknown error occured";
-
-    //Type guarding, making sure error is an actual Error
-    if (error instanceof Error) errorMessage = error.message;
-
-    return NextResponse.json({ error: errorMessage }, { status: 500 });
-  }
-}
+// // Initialize Mailgun client with your API key and domain
+// const mg = mailgun({
+//   apiKey: "MAILGUN_API_KEY",
+//   domain: "YOUR_MAILGUN_DOMAIN",
+// });
--- ericleecodeswebsite/app/emails/ContactMe.tsx
+++ ericleecodeswebsite/app/emails/ContactMe.tsx
@@ -1,19 +1,22 @@
-import { Button, Html } from "@react-email/components";
+import { Html } from "@react-email/components";
 import * as React from "react";
 
 type ContactMeProps = {
   firstName: string;
+  message: string;
+  email: string;
 };
 
-export default function ContactMe({ firstName }: ContactMeProps) {
+export default function ContactMe({
+  firstName,
+  message,
+  email,
+}: ContactMeProps) {
   return (
     <Html>
-      <Button
-        href="https://example.com"
-        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
-      >
-        Click me
-      </Button>
+      <p>{message}</p>
+      <p>From, {firstName}</p>
+      <p>Email: {email}</p>
     </Html>
   );
 }
