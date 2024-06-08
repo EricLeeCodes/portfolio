f7b81b443a39db870897a1146f7686386ef9277c
Eric Lee <ericleecodes@gmail.com>
2024-06-07 17:52:37 -0700

    Removed route.ts for deploying purpose

--- ericleecodeswebsite/app/api/send/route.ts
+++
@@ -1,7 +0,0 @@
-// import mailgun from "mailgun-js";
-
-// // Initialize Mailgun client with your API key and domain
-// const mg = mailgun({
-//   apiKey: "MAILGUN_API_KEY",
-//   domain: "YOUR_MAILGUN_DOMAIN",
-// });
