c95090dab02a36af09db59325b2450ec31f602d4
Eric Lee <ericleecodes@gmail.com>
2024-09-09 22:01:23 -0700

    Added work schema to site

--- ericleecodeswebsite/sanity/schemas/index.tsx
+++ ericleecodeswebsite/sanity/schemas/index.tsx
@@ -1,7 +1,8 @@
 import project from "./project-schema";
 import page from "./page-schema";
+import work from "./work-schema";
 
-const schemas = [project, page];
+const schemas = [project, page, work];
 // const schemas = [project];
 
 export default schemas;
